2f1eb74af663fe40fc20689a458a407eb7c804f4
Erik Svennberg <erik.svennberg@student.tabyenskilda.se>
2024-01-16 13:53:00 +0100

    javascript lek

---
+++ script.js
@@ -0,0 +1,18 @@
+/*byt bild/logga */
+function byttillbild() {
+    document.getElementById('logga').src = 'img/arushee-agrawal-pU0a4vYJwco-unsplash.webp';
+    }
+    function byttilllogga() {
+    document.getElementById('logga').src = 'img/felix-rottmann-Uvx36KXixh8-unsplash.jpg';
+    }
+    /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
